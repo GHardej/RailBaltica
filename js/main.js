e4d99594884935cdac0ca9d2c69deece08f67d7a
Grzegorz Hardejewicz <ghardejewicz@gmail.com>
2022-02-17 21:38:00 +0100

    HEADER: added functionality for CTA button, code optimization, visual changes in layout

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-// const actualDay = actualDate.getDate();
-// const actualMonth = actualDate.getMonth();
-// const actualYear = actualDate.getFullYear();
 const nav = document.querySelector('.nav');
 const navItemsHover = document.querySelectorAll('.nav__item-hover');
-const btnIcon = document.querySelector('.btn');
+const btnNav = document.querySelector('.btn');
+const btnCTA = document.querySelector('.header__btn');
 const weatherDate = document.querySelector('.header__weather-date');
 const temperature = document.querySelector('.header__weather-temp');
 const weatherPhoto = document.querySelector('.header__weather-photo');
 
 // contract days calculation
-const actualDate = new Date();
-const startDate = new Date(2020, 05, 27);
-const endDate = new Date(2023, 06, 27);
-
 const barDaysElapsed = document.querySelector('.progress__bar-days-elapsed');
 const daysElapsedPercentageInfo = document.querySelector(
 	'.progress__bar-days-elapsed-percentage'
@@ -23,21 +17,20 @@ const daysToFinishInfo = document.querySelector(
 	'.progress__info-days-to-finish'
 );
 
+const actualDate = new Date();
+const startDate = new Date(2020, 05, 27);
+const endDate = new Date(2023, 06, 27);
 const contractDuration = endDate.getTime() - startDate.getTime();
 
 const elapsedDays = Math.ceil(
-	(actualDate.getTime() - startDate.getTime()) /
-	(24 * 60 * 60 * 1000)
+	(actualDate.getTime() - startDate.getTime()) / (24 * 60 * 60 * 1000)
 );
-
 const daysElapsedPercentage = Math.round(
-	((actualDate.getTime() - startDate.getTime()) / contractDuration) *
-	100
+	((actualDate.getTime() - startDate.getTime()) / contractDuration) * 100
 );
-
 const daysToFinish = Math.ceil(
 	(endDate.getTime() - actualDate.getTime() + 24 * 60 * 60 * 1000) /
-	(24 * 60 * 60 * 1000)
+		(24 * 60 * 60 * 1000)
 );
 
 barDaysElapsed.style.width = daysElapsedPercentage + '%';
@@ -47,53 +40,17 @@ daysElapsedInfo.textContent = elapsedDays;
 daysToFinishInfo.textContent = daysToFinish;
 daysElapsedPercentageInfo.textContent = daysElapsedPercentage + '%';
 
-console.log(elapsedDays);
-console.log(daysElapsedPercentage);
-console.log(daysToFinish);
-
-
-// elapsedDays.textContent =
-// 	(
-// 		(actualDate.getTime() - startDate.getTime()) /
-// 		(24 * 60 * 60 * 1000)
-// 	).toFixed() + ' dni ';
-
-// elapsedDaysPercentage.textContent =
-// 	'(' +
-// 	(
-// 		((actualDate.getTime() - startDate.getTime()) / contractDuration) *
-// 		100
-// 	).toFixed() +
-// 	'%)';
-
-// daysToFinish.textContent =
-// 	(
-// 		(endDate.getTime() - actualDate.getTime() + 24 * 60 * 60 * 1000) /
-// 		(24 * 60 * 60 * 1000)
-// 	).toFixed() + ' dni ';
-
-// daysToFinishPercentage.textContent =
-// 	'(' +
-// 	(
-// 		((endDate.getTime() - actualDate.getTime()) / contractDuration) *
-// 		100
-// 	).toFixed() +
-// 	'%)';
-
 // weather API
+const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+weatherDate.textContent = actualDate.toLocaleDateString('pl-PL', dateOptions);
+
 const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const API_KEY = '&appid=e98e00972c6e8d5388bb75b96e2d5b48';
 const API_UNITS = '&units=metric';
 
-// const URL = 'https://danepubliczne.imgw.pl/api/data/synop/id/12295'
-// fetch(URL).then(res => res.json()).then(data => console.log(data))
-
 const getWeather = () => {
 	const city = 'Białystok';
 	const URL = API_LINK + city + API_KEY + API_UNITS;
-
-	const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-	weatherDate.textContent = actualDate.toLocaleDateString('pl-PL', dateOptions);
 
 	fetch(URL)
 		.then((res) => res.json())
@@ -130,14 +87,7 @@ const getWeather = () => {
 
 getWeather();
 
-// console.log(
-// 	actualDate.getDate() +
-// 		' ' +
-// 		monthsArr[actualDate.getMonth()] +
-// 		' ' +
-// 		actualDate.getFullYear()
-// );
-
+// activate/desactivate menu
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
 
@@ -146,4 +96,17 @@ const handleNav = () => {
 	});
 };
 
-btnIcon.addEventListener('click', handleNav);
+// CTA button onclick functions
+const btnClick = () => {
+	btnCTA.classList.add('header__btn--click');
+};
+const btnRelease = () => {
+	btnCTA.classList.remove('header__btn--click');
+};
+
+// listeners
+
+btnCTA.addEventListener('mousedown', btnClick);
+btnCTA.addEventListener('mouseup', btnRelease);
+
+btnNav.addEventListener('click', handleNav);
